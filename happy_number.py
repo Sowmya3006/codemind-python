aef44231ebd11fa2fe37435b9ced32b71f7e3139
Sowmya3006 <89642540+Sowmya3006@users.noreply.github.com>
2022-10-29 11:44:39 +0530

    def happy(n):     if n<9:         return n     else:         s=0         while n!=0:             r=n%10             s+=r*r             n=n//10         return s n=int(input()) while n>9:     n=happy(n) if n==1 or n==7:     print("True") else:     print("False")

---
+++ happy_number.py
@@ -0,0 +1,17 @@
+def happy(n):
+    if n<9:
+        return n
+    else:
+        s=0
+        while n!=0:
+            r=n%10
+            s+=r*r
+            n=n//10
+        return s 
+n=int(input())
+while n>9:
+    n=happy(n)
+if n==1 or n==7:
+    print("True")
+else:
+    print("False")
